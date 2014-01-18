f72ac67baa31160b8adb5ffea9da379b3783a837
diegoojedagarcia@gmail.com <diegoojedagarcia@gmail.com@7a3a527a-1539-2c17-4610-1faed607ce3e>
2014-01-18 11:02:00 +0000

    Inicialización

---
+++ iGenda/src/java/servicios/service/GrupoFacadeREST.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package servicios.service;
+
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import servicios.Grupo;
+
+/**
+ *
+ * @author diegoojedagarcia
+ */
+@Stateless
+@Path("servicios.grupo")
+public class GrupoFacadeREST extends AbstractFacade<Grupo> {
+    @PersistenceContext(unitName = "iGendaPU")
+    private EntityManager em;
+
+    public GrupoFacadeREST() {
+        super(Grupo.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({"application/json"})
+    public void create(Grupo entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({"application/json"})
+    public void edit(@PathParam("id") String id, Grupo entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") String id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({"application/json"})
+    public Grupo find(@PathParam("id") String id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({"application/json"})
+    public List<Grupo> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({"application/json"})
+    public List<Grupo> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces("text/plain")
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
