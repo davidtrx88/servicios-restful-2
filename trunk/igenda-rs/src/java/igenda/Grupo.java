cca0dfae688a7fc9776fd4549b9ed37e5ad32ae9
diegoojedagarcia@gmail.com <diegoojedagarcia@gmail.com@7a3a527a-1539-2c17-4610-1faed607ce3e>
2014-01-21 19:18:31 +0000

    asdf

---
+++ trunk/igenda-rs/src/java/igenda/Grupo.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package igenda;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author diegoojedagarcia
+ */
+@Entity
+@Table(name = "GRUPO")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Grupo.findAll", query = "SELECT g FROM Grupo g"),
+    @NamedQuery(name = "Grupo.findByIdgrupo", query = "SELECT g FROM Grupo g WHERE g.idgrupo = :idgrupo"),
+    @NamedQuery(name = "Grupo.findByNombregrupo", query = "SELECT g FROM Grupo g WHERE g.nombregrupo = :nombregrupo")})
+public class Grupo implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 200)
+    @Column(name = "IDGRUPO")
+    private String idgrupo;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "NOMBREGRUPO")
+    private String nombregrupo;
+    @OneToMany(mappedBy = "grupo")
+    private Collection<Contacto> contactoCollection;
+    @JoinColumn(name = "LOGIN", referencedColumnName = "LOGIN")
+    @ManyToOne(optional = false)
+    private Usuario login;
+
+    public Grupo() {
+    }
+
+    public Grupo(String idgrupo) {
+        this.idgrupo = idgrupo;
+    }
+
+    public Grupo(String idgrupo, String nombregrupo) {
+        this.idgrupo = idgrupo;
+        this.nombregrupo = nombregrupo;
+    }
+
+    public String getIdgrupo() {
+        return idgrupo;
+    }
+
+    public void setIdgrupo(String idgrupo) {
+        this.idgrupo = idgrupo;
+    }
+
+    public String getNombregrupo() {
+        return nombregrupo;
+    }
+
+    public void setNombregrupo(String nombregrupo) {
+        this.nombregrupo = nombregrupo;
+    }
+
+    @XmlTransient
+    public Collection<Contacto> getContactoCollection() {
+        return contactoCollection;
+    }
+
+    public void setContactoCollection(Collection<Contacto> contactoCollection) {
+        this.contactoCollection = contactoCollection;
+    }
+
+    public Usuario getLogin() {
+        return login;
+    }
+
+    public void setLogin(Usuario login) {
+        this.login = login;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idgrupo != null ? idgrupo.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Grupo)) {
+            return false;
+        }
+        Grupo other = (Grupo) object;
+        if ((this.idgrupo == null && other.idgrupo != null) || (this.idgrupo != null && !this.idgrupo.equals(other.idgrupo))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "igenda.Grupo[ idgrupo=" + idgrupo + " ]";
+    }
+    
+}
